519897c008bf08ab6bdd93c3e855e986d97c81ce
Carmela Acevedo <kar288@gmail.com>
2016-01-16 22:51:38 +0100

    setting up barcode scanning

--- gettingstarted/static/build/index.js
+++ gettingstarted/static/build/index.js
@@ -80,17 +80,17 @@ class Main extends React.Component {
 
     Quagga.onDetected(function (result) {
       var code = result.codeResult.code;
-      that.nextStep({ code: code });
-      console.log(code);
-      // if (App.lastResult !== code) {
-      //     App.lastResult = code;
-      //     var $node = null, canvas = Quagga.canvas.dom.image;
-      //
-      //     $node = $('<li><div class="thumbnail"><div class="imgWrapper"><img /></div><div class="caption"><h4 class="code"></h4></div></div></li>');
-      //     $node.find("img").attr("src", canvas.toDataURL());
-      //     $node.find("h4.code").html(code);
-      //     $("#result_strip ul.thumbnails").prepend($node);
-      // }
+      if (App.lastResult !== code) {
+        App.lastResult = code;
+        that.nextStep({ code: code });
+        console.log(code);
+        //     var $node = null, canvas = Quagga.canvas.dom.image;
+        //
+        //     $node = $('<li><div class="thumbnail"><div class="imgWrapper"><img /></div><div class="caption"><h4 class="code"></h4></div></div></li>');
+        //     $node.find("img").attr("src", canvas.toDataURL());
+        //     $node.find("h4.code").html(code);
+        //     $("#result_strip ul.thumbnails").prepend($node);
+      }
     });
   }
 
